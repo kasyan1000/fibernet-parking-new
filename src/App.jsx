f7cc11f70562f85c59239f57474d39aed57c6004
kasyan1000 <kasyan1000@gmail.com>
2025-04-10 12:50:16 +0300

    Add files via upload
    עדכון קבצים

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>ברוך הבא למערכת הזמנת חניות אורחים של פייברנט</h1>
+      <p>האתר פועל! 🚗</p>
+    </div>
+  );
+}
