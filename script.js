0406cdf5b9d4421d437c156ff1529e1e6a691d77
kasyan1000 <kasyan1000@gmail.com>
2025-04-11 22:00:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function showLoginPopup() {
+  document.getElementById("login-popup").style.display = "block";
+}
+
+function hideLoginPopup() {
+  document.getElementById("login-popup").style.display = "none";
+}
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin officer" && password === "Parkfor@56789") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("פרטי ההתחברות שגויים.");
+  }
+}
